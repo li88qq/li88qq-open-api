7f1e083e06ea39a2739bbfe23c6afa5397a9f688
li88qq <573258011@qq.com>
2023-08-29 22:04:03 +0800

    调整init.sql

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -7,5 +7,8 @@ values (1, '我的', 0, null, null, null, null, 1, -1),
        (3, '工作台', 1, '1', '/my/workbench', null, null, 2, -1),
        (4, '设置', 1, '1', '/my/setting', null, null, 3, -1),
        (5, '记录', 1, '1', null, null, null, 4, -1),
-       (6, '登录记录', 5, '1,5', '/my/loginlog', null, null, 1, -1),
-       (7, '操作记录', 5, '1,5', '/my/actionlog', null, null, 2, -1);
+       (6, '登录记录', 5, '1,5', '/log/login', null, null, 1, -1),
+       (7, '操作记录', 5, '1,5', '/log/action', null, null, 2, -1),
+       (8, '系统管理', 0, NULL, NULL, '', NULL, 2, 0),
+       (9, '用户管理', 8, '8', '/system/user', '', NULL, 1, 0),
+       (10, '菜单管理', 8, '8', '/system/menu', '', NULL, 2, 0)
